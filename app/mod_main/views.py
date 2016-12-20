293690bd31ba8fa7947cdf550979edcb1d8401c3
root <root@localhost.localdomain>
2016-12-20 10:06:55 -0500

    forma gjysme e perfunduar

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,10 +1,50 @@
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template , request , redirect , url_for
+from bson import json_util , ObjectId
+from app import mongo
+from arkep import MyForm
+
 mod_main = Blueprint('main', __name__)
 
 
-@mod_main.route('/', methods=['GET'])
+@mod_main.route ('/' ,methods=['GET' , 'POST'])
 def index():
-    ''' Renders the App index page.
-    :return:
-    '''
-    return render_template("index.html")
+
+    db = mongo.db.arkep
+
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method =='POST':
+        data = request.form.to_dict()
+        db.insert(data)
+        return json_util.dumps(data)
+    else:
+        return 'bad request'
+
+
+@mod_main.route('/<string:id>', methods=['GET'])
+def get_doc(id):
+    db = mongo.db.arkep
+
+    if request.method == 'GET':
+        doc = db.find({"_id":ObjectId(id)})
+        doc_json = json_util.dumps(doc)
+        return  render_template('doc.html' , doc=doc_json)
+    else:
+        return "bat request"
+
+
+
+@mod_main.route('/remove/<string:id>', methods=['GET'])
+def remove_doc(id):
+    db = mongo.db.arkep
+    if request.method == 'GET':
+        doc = db.remove({"_id":ObjectId(id)})
+        return redirect(url_for('main.lista'))
+    else:
+        return "bat request"
+
+
+@mod_main.route('/lista' , methods=['GET'])
+def lista():
+    dokumentat = mongo.db.arkep.find()
+    return render_template('lista.html' , documentat = dokumentat)
